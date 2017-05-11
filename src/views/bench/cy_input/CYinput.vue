5c04b0c4941b738d44ae59aedcc7cc19e775b26b
baiyang <459676797@qq.com>
2017-05-11 21:42:08 +0800

    123

---
+++ src/views/bench/cy_input/CYinput.vue
@@ -0,0 +1,66 @@
+<template>
+	<el-form ref="form" :model="form" label-width="80px" @submit.prevent="onSubmit" style="margin:20px;width:60%;min-width:600px;">
+		<el-form-item label="样品类型">
+			<el-select v-model="form.region" placeholder="样品类型">
+				<el-option label="区域一" value="shanghai"></el-option>
+				<el-option label="区域二" value="beijing"></el-option>
+			</el-select>
+		</el-form-item>
+		<el-form-item label="样品名称">
+			<el-select v-model="form.region" placeholder="请选择样品名称">
+				<el-option label="区域一" value="shanghai"></el-option>
+				<el-option label="区域二" value="beijing"></el-option>
+			</el-select>
+		</el-form-item>
+		<el-form-item label="摊位号">
+			<el-select v-model="form.region" placeholder="摊位号">
+				<el-option label="区域一" value="shanghai"></el-option>
+				<el-option label="区域二" value="beijing"></el-option>
+			</el-select>
+		</el-form-item>
+		<el-form-item label="经营户">
+			baiyang
+		</el-form-item>
+		<el-form-item label="采样人员">
+			baiyang
+		</el-form-item>
+		<el-form-item label="采样地点">
+			baiyang
+		</el-form-item>
+		<el-form-item label="采样时间">
+			<el-col :span="11">
+				<el-date-picker type="date" placeholder="选择日期" v-model="form.date1" style="width: 100%;"></el-date-picker>
+			</el-col>
+		</el-form-item>
+
+		<el-form-item>
+			<el-button type="primary">立即创建</el-button>
+			<el-button @click.native.prevent>取消</el-button>
+		</el-form-item>
+	</el-form>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				form: {
+					name: '',
+					region: '',
+					date1: '',
+					date2: '',
+					delivery: false,
+					type: [],
+					resource: '',
+					desc: ''
+				}
+			}
+		},
+		methods: {
+			onSubmit() {
+				console.log('submit!');
+			}
+		}
+	}
+
+</script>
